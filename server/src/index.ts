a262ba03551d15d5db9ef0c13c79462565f2f42a
Mutesa-Cedric <mutesacedric@gmail.com>
2022-07-13 16:49:57 +0200

    feat : backend-frontend connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello from ts");
+import express from "express";
+require('dotenv').config();
+const cors=require('cors');
+const app=express();
+app.use(cors());
+const port = process.env.PORT ||3200
+app.get('/',(req,res)=>{
+    res.json({
+        sucess:true,
+        message:"testing backend reload"
+    })
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
